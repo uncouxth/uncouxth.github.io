789a11cb45ab2c0801f62926ab5198c56486aaf5
Nikhi Bhambra <nikhibhambra@gmail.com>
2023-03-02 23:20:49 -0500

    Add initial door anim

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
-// let myThing = anime({
-//    targets: 'div.door',
-//    translateY: [
-//      { value: 200, duration: 500 },
-//      { value: 0, duration: 800 }
-//    ]
-//  });
+anime({
+    targets: '.door2',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'cubicBezier(0.00, 0.01, 0.350, 1.020)',
+    duration: 2150,
+    delay: 400,
+    loop: false
+  });
+
+anime({
+    targets: '.knob',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'cubicBezier(0.00, 0.01, 0.350, 1.020)',
+    duration: 500,
+    delay: 2050,
+    loop: false
+  });
